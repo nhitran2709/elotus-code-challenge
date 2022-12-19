f819d4e599433e2c642c552d1531d780f8eb855a
Thuong Nguyen <yennhi2709.it@gmail.com>
2022-12-19 23:45:04 +0700

    Created SearchInput component and style for it

---
+++ code-challenge/src/components/SearchInput/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Input } from 'antd';
+
+import './styles.css';
+
+const SearchInput = () => <Input className='movie-search-input' placeholder="Search movie" />;
+
+export default SearchInput;
--- code-challenge/src/screens/Homepage.tsx
+++ code-challenge/src/screens/Homepage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header.tsx";
 import MovieCollection from "../components/MovieCollection";
 import MovieTabBarLayout from "../components/MovieTabBar/MovieTabBarLayout";
+import SearchInput from "../components/SearchInput";
 
 const HomeScreen = () => {
   return (
     <div>
       <Header />
       <MovieCollection />
+      <SearchInput />
       <MovieTabBarLayout />
     </div>
   )
