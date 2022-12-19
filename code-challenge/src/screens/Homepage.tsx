bc73321bf431eda79edb3b98e9c02eea625ce3cb
Thuong Nguyen <yennhi2709.it@gmail.com>
2022-12-19 23:39:16 +0700

    Create Movie Collection Component and style

--- code-challenge/src/screens/Homepage.tsx
+++ code-challenge/src/screens/Homepage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header.tsx";
+import MovieCollection from "../components/MovieCollection";
 import MovieTabBarLayout from "../components/MovieTabBar/MovieTabBarLayout";
 
 const HomeScreen = () => {
   return (
     <div>
       <Header />
+      <MovieCollection />
       <MovieTabBarLayout />
     </div>
   )
